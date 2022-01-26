a3e68ffdf2e56f29be6886e18de7a7c66abb2dc2
Aaron Yu <aaron.yuu@outlook.com>
2022-01-26 22:11:18 +0800

    add test demo into pkg.

--- pkg/hello/hello_test.go
+++ pkg/hello/hello_test.go
@@ -3,10 +3,9 @@ package hello
 import "testing"
 
 func Test_Greet_Name(t *testing.T) {
-	result := Greet("Annie")
 	expect := "Hello! Annie."
 
-	if result != expect {
+	if result := Greet("Annie"); result != expect {
 		t.Errorf("Greet(name string) = %s; want %s", result, expect)
 	}
 }
